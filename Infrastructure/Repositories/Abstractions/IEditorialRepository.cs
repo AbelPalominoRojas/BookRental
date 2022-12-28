14e1784f0d659bcff5409768f1c3a109807127df
AbelPalominoRojas <abelpr87@gmail.com>
2022-12-28 12:44:46 -0500

    Editorial Repository crud

--- Infrastructure/Repositories/Abstractions/IEditorialRepository.cs
+++ Infrastructure/Repositories/Abstractions/IEditorialRepository.cs
@@ -5,6 +5,10 @@ namespace Infrastructure.Repositories.Abstractions
 {
 	public interface IEditorialRepository
 	{
+		Task<Editorial> Create(Editorial entity);
+		Task<Editorial?> Edit(int id, Editorial entity);
+		Task<Editorial?> EnableOrDisable(int id);
+		Task<Editorial?> Find(int id);
 		Task<IList<Editorial>> FindAll();
 	}
 }
--- Infrastructure/Repositories/Implementations/EditorialRepository.cs
+++ Infrastructure/Repositories/Implementations/EditorialRepository.cs
@@ -15,6 +15,49 @@ namespace Infrastructure.Repositories.Implementations
             _context = context;
         }
 
+        public async Task<Editorial> Create(Editorial entity)
+        {
+            _context.Editoriales.Add(entity);
+
+            await _context.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public async Task<Editorial?> Edit(int id, Editorial entity)
+        {
+            var model = await _context.Editoriales.FindAsync(id);
+
+            if (model != null)
+            {
+                model.Codigo = entity.Codigo;
+                model.Nombre = entity.Nombre;
+
+                _context.Editoriales.Update(model);
+                await _context.SaveChangesAsync();
+            }
+
+            return model;
+        }
+
+        public async Task<Editorial?> EnableOrDisable(int id)
+        {
+            var model = await _context.Editoriales.FindAsync(id);
+
+            if (model != null)
+            {
+                model.Estado = (model.Estado == 0) ? 1 : 0;
+
+                _context.Editoriales.Update(model);
+                await _context.SaveChangesAsync();
+            }
+
+            return model;
+        }
+
+        public async Task<Editorial?> Find(int id)
+        => await _context.Editoriales.FindAsync(id);
+
         public async Task<IList<Editorial>> FindAll()
         => await _context.Editoriales.OrderByDescending(e => e.Id).ToListAsync();
     }
