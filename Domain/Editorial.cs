6cb7d2fc88437fc25068426e47f63e65c4d32749
AbelPalominoRojas <abelpr87@gmail.com>
2022-12-28 09:50:02 -0500

    Add Db context and connectio

--- Domain/Editorial.cs
+++ Domain/Editorial.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 namespace Domain
 {
-	public class Editorial
-	{
-		public Editorial()
-		{
-		}
-	}
+    public class Editorial
+    {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public DateTime FechaRegistro { get; set; } = DateTime.UtcNow;
+        public int Estado { get; set; } = 1;
+    }
 }
 
---
+++ Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Context
+{
+	public class ApplicationDbContext : DbContext
+	{
+
+		public ApplicationDbContext()
+		{
+		}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+    }
+}
+
