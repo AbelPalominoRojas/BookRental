943f4443191203f8141d58b2695bfd93512c5076
AbelPalominoRojas <abelpr87@gmail.com>
2022-12-28 18:46:12 -0500

    Utils Pagination

--- Utils/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Utils;
-public class Class1
-{
-
-}
-
---
+++ Utils/Paginations/ResponsePagination.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Utils.Paginations
+{
+    public class ResponsePagination<T> : Pagination
+    {
+        public ResponsePagination()
+        {
+        }
+
+        public ResponsePagination(int total, int page, int perPage) : base(total, page, perPage) { }
+
+        public ResponsePagination(Pagination pagination) : base(pagination) { }
+
+        public IList<T> Data { get; set; }
+    }
+}
+
