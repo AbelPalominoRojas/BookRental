666879c29c26105d2a476245cb62c00ee07bc311
AbelPalominoRojas <abelpr87@gmail.com>
2022-12-28 10:16:41 -0500

    Add EditorialMap

--- Infrastructure/Context/ApplicationDbContext.cs
+++ Infrastructure/Context/ApplicationDbContext.cs
@@ -1,18 +1,23 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace Infrastructure.Context
 {
 	public class ApplicationDbContext : DbContext
 	{
+        private readonly IConfiguration _configuration;
 
-		public ApplicationDbContext()
+		public ApplicationDbContext(IConfiguration configuration)
 		{
+            _configuration = configuration;
 		}
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DBConnection"));
         }
 
     }
---
+++ Infrastructure/ModelMaps/EditorialMap.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Domain;
+
+namespace Infrastructure.ModelMaps
+{
+    public class EditorialMap : IEntityTypeConfiguration<Editorial>
+    {
+        public void Configure(EntityTypeBuilder<Editorial> builder)
+        {
+            builder.ToTable("editoriales");
+            builder.HasKey(t => t.Id);
+            builder.Property(t => t.Id).HasColumnName("id");
+            builder.Property(t => t.Codigo).HasColumnName("codigo");
+            builder.Property(t => t.Nombre).HasColumnName("nombre");
+            builder.Property(t => t.FechaRegistro).HasColumnName("fecha_registro");
+            builder.Property(t => t.Estado).HasColumnName("estado");
+        }
+    }
+}
+
