b88bce6e120407df1ef9d56745bbfc8cac909b83
AbelPalominoRojas <abelpr87@gmail.com>
2022-12-28 11:23:09 -0500

    Editorial controller - List Method

---
+++ WebApi/Controllers/EditorialController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Dtos.Editoriales;
+using Application.Services.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class EditorialController : Controller
+    {
+        private readonly IEditorialService _editorialService;
+
+        public EditorialController(IEditorialService editorialService)
+        {
+            _editorialService = editorialService;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<EditorialDto>> Get()
+        => await _editorialService.FindAll();
+    }
+}
+
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,4 +1,10 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Application.Services.Abstractions;
+using Application.Services.Implementations;
+using Infrastructure.Context;
+using Infrastructure.Repositories.Abstractions;
+using Infrastructure.Repositories.Implementations;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
@@ -6,6 +12,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Data base context
+builder.Services.AddDbContext<ApplicationDbContext>();
+
+// DI
+builder.Services.AddScoped<IEditorialRepository, EditorialRepository>();
+
+builder.Services.AddScoped<IEditorialService, EditorialService>();
+
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
