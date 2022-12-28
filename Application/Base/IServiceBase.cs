e8c5772c727a214443489db52a111b04b8eff2ff
AbelPalominoRojas <abelpr87@gmail.com>
2022-12-28 16:38:33 -0500

    Add IService Base

---
+++ Application/Base/IServiceBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Application.Base
+{
+    public interface IServiceBase<TDto, TFormDto, K>
+    {
+        Task<TDto> Create(TFormDto dto);
+        Task<TDto?> Edit(int id, TFormDto dto);
+        Task<TDto?> EnableOrDisable(int id);
+        Task<TDto?> Find(int id);
+        Task<IList<TDto>> FindAll();
+    }
+}
+
--- Application/Services/Abstractions/IEditorialService.cs
+++ Application/Services/Abstractions/IEditorialService.cs
@@ -1,15 +1,11 @@
 ﻿using System;
+using Application.Base;
 using Application.Dtos.Editoriales;
 
 namespace Application.Services.Abstractions
 {
-	public interface IEditorialService
-	{
-		Task<EditorialDto> Create(EditorialFormDto dto);
-		Task<EditorialDto?> Edit(int id, EditorialFormDto dto);
-		Task<EditorialDto?> EnableOrDisable(int id);
-		Task<EditorialDto?> Find(int id);
-		Task<IList<EditorialDto>> FindAll();
-	}
+    public interface IEditorialService : IServiceBase<EditorialDto, EditorialFormDto, int>
+    {
+    }
 }
 
