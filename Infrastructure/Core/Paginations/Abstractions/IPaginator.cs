c6f1c183e397d5855ce827786745833f4a997593
AbelPalominoRojas <abelpr87@gmail.com>
2022-12-28 19:05:26 -0500

    Add Core Paginator

---
+++ Infrastructure/Core/Paginations/Abstractions/IPaginator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Utils.Paginations;
+namespace Infrastructure.Core.Paginations.Abstractions
+{
+	public interface IPaginator<T>
+	{
+		Task<ResponsePagination<T>> Paginate(IQueryable<T> query, RequestPagination<T> request);
+	}
+}
+
---
+++ Infrastructure/Core/Paginations/Implementations/Paginator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Infrastructure.Core.Paginations.Abstractions;
+using Microsoft.EntityFrameworkCore;
+using Utils.Paginations;
+
+namespace Infrastructure.Core.Paginations.Implementations
+{
+    public class Paginator<T> : IPaginator<T>
+    {
+        public async Task<ResponsePagination<T>> Paginate(IQueryable<T> query, RequestPagination<T> request)
+        {
+            var total = await query.CountAsync();
+            var pagination = new Pagination(total, request.Page, request.Page);
+
+            var sizePerPage = pagination.PerPage;
+
+            var diference = (pagination.To - pagination.From) + 1;
+            if (diference < pagination.PerPage) sizePerPage = diference;
+
+            var currentPage = pagination.CurrentPage;
+            if (currentPage > 0) currentPage = pagination.CurrentPage - 1;
+
+
+            query = query.Skip(currentPage * sizePerPage).Take(sizePerPage);
+            var data = await query.ToListAsync();
+
+            var response = new ResponsePagination<T>(pagination)
+            {
+                Data = data
+            };
+
+            return response;
+        }
+    }
+}
+
