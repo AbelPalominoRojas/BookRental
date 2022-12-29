876bb9e40457d36e1bfe03db2d52a250b4019699
AbelPalominoRojas <abelpr87@gmail.com>
2022-12-29 08:37:48 -0500

    Solicitante and Prestamo Implemented

---
+++ WebApi/Controllers/PrestamoController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Dtos.Prestamos;
+using Application.Services.Abstractions;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Utils.Paginations;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class PrestamoController : Controller
+    {
+        private readonly IPrestamoService _prestamoService;
+
+        public PrestamoController(IPrestamoService prestamoService)
+        {
+            _prestamoService = prestamoService;
+        }
+
+
+        [HttpGet]
+        public async Task<IEnumerable<PrestamoDto>> Get()
+        => await _prestamoService.FindAll();
+
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(PrestamoDto))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<Results<NotFound, Ok<PrestamoDto>>> Get(int id)
+        {
+            var response = await _prestamoService.Find(id);
+
+            if (response == null) return TypedResults.NotFound();
+
+            return TypedResults.Ok(response);
+        }
+
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(PrestamoDto))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<Results<BadRequest, Ok<PrestamoDto>>> Post([FromBody] PrestamoFormDto request)
+        {
+            var response = await _prestamoService.Create(request);
+
+            if (response == null) return TypedResults.BadRequest();
+
+            return TypedResults.Ok(response);
+        }
+
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(PrestamoDto))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<Results<BadRequest, NotFound, Ok<PrestamoDto>>> Put(int id, [FromBody] PrestamoFormDto request)
+        {
+            var response = await _prestamoService.Edit(id, request);
+
+            if (response == null) return TypedResults.NotFound();
+
+            return TypedResults.Ok(response);
+        }
+
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(PrestamoDto))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<Results<NotFound, Ok<PrestamoDto>>> Delete(int id)
+        {
+            var response = await _prestamoService.EnableOrDisable(id);
+
+            if (response == null) return TypedResults.NotFound();
+
+            return TypedResults.Ok(response);
+        }
+
+
+        [HttpGet("PaginatedSearch")]
+        public async Task<ResponsePagination<PrestamoDto>> PaginatedSearch([FromQuery] RequestPagination<PrestamoDto> request)
+        => await _prestamoService.PaginatedSearch(request);
+    }
+}
+
---
+++ WebApi/Controllers/SolicitanteController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Dtos.Solicitantes;
+using Application.Services.Abstractions;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Utils.Paginations;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class SolicitanteController : Controller
+    {
+        private readonly ISolicitanteService _solicitanteService;
+
+        public SolicitanteController(ISolicitanteService solicitanteService)
+        {
+            _solicitanteService = solicitanteService;
+        }
+
+
+        [HttpGet]
+        public async Task<IEnumerable<SolicitanteDto>> Get()
+        => await _solicitanteService.FindAll();
+
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(SolicitanteDto))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<Results<NotFound, Ok<SolicitanteDto>>> Get(int id)
+        {
+            var response = await _solicitanteService.Find(id);
+
+            if (response == null) return TypedResults.NotFound();
+
+            return TypedResults.Ok(response);
+        }
+
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(SolicitanteDto))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<Results<BadRequest, Ok<SolicitanteDto>>> Post([FromBody] SolicitanteFormDto request)
+        {
+            var response = await _solicitanteService.Create(request);
+
+            if (response == null) return TypedResults.BadRequest();
+
+            return TypedResults.Ok(response);
+        }
+
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(SolicitanteDto))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<Results<BadRequest, NotFound, Ok<SolicitanteDto>>> Put(int id, [FromBody] SolicitanteFormDto request)
+        {
+            var response = await _solicitanteService.Edit(id, request);
+
+            if (response == null) return TypedResults.NotFound();
+
+            return TypedResults.Ok(response);
+        }
+
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(SolicitanteDto))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<Results<NotFound, Ok<SolicitanteDto>>> Delete(int id)
+        {
+            var response = await _solicitanteService.EnableOrDisable(id);
+
+            if (response == null) return TypedResults.NotFound();
+
+            return TypedResults.Ok(response);
+        }
+
+
+        [HttpGet("PaginatedSearch")]
+        public async Task<ResponsePagination<SolicitanteDto>> PaginatedSearch([FromQuery] RequestPagination<SolicitanteDto> request)
+        => await _solicitanteService.PaginatedSearch(request);
+    }
+}
+
