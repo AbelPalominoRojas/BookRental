b70c07cbf3a840cf11f0e21afba829867849a9dc
AbelPalominoRojas <abelpr87@gmail.com>
2023-01-11 08:39:06 -0500

    Refactoring RequestPagination

--- Utils/Paginations/RequestPagination.cs
+++ Utils/Paginations/RequestPagination.cs
@@ -1,22 +1,14 @@
 ﻿using System;
 namespace Utils.Paginations
 {
-	public class RequestPagination<T>
-	{
-		private int _page = 1;
-		private int _perPag;
+    public class RequestPagination<T>
+    {
+        private int _page = 1;
+        private int _perPag;
 
-		public int Page
-		{
-			get { return (_page <= 0) ? 1 : _page; }
-			set { _page = value; }
-		}
-		public int PerPage
-		{
-			get { return (_perPag <= 0) ? 10 : _perPag; }
-			set { _perPag = value; }
-		}
-		public T? Filter { get; set; }
-	}
+        public int Page { get => _page <= 0 ? 1 : _page; set => _page = value; }
+        public int PerPage { get => _perPag <= 0 ? 10 : _perPag; set => _perPag = value; }
+        public T? Filter { get; set; }
+    }
 }
 
